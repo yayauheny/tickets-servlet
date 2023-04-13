e463002e8660b4f86892279bc615df2cd2f0def7
Evgeny Leshok <work.evles@gmail.com>
2023-04-13 16:02:25 +0300

    refactor: fix problems with liquibase

--- src/main/resources/db.changelog/changeset/create-discount-card-table.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
-
---changeset yayauheny:3
-CREATE TABLE IF NOT EXISTS company.discount_card
-(
-    id       SERIAL PRIMARY KEY,
-    discount DECIMAL NOT NULL
-);
--- src/main/resources/db.changelog/changeset/create-schema.sql
+++
@@ -1,4 +0,0 @@
---liquibase formatted sql
-
---changeset yayauheny:1
-CREATE SCHEMA IF NOT EXISTS company;
